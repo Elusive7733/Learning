9a822da082a4545aea84cba73bffd3eeafe88c5d
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-01-18 18:28:36 +0500

    Edited files

---
+++ PyMP and OpenMP/Q1_OpenMP.c
@@ -0,0 +1,68 @@
+#include <math.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+long double x, fact[150], pwr[150];
+
+
+void power(){
+    int i;
+    for(i = 0; i < 150; i++){
+        pwr[i] = pow(x, i);
+    }
+}
+
+void factorial(){
+    long double num;
+    int i, j;
+    fact[0] = 1.0;
+    for(i = 1; i < 150; i++){
+        num = 1.0;
+        for(j = i; j > 0; j--){
+            num = num * j;
+        }
+        fact[i] = num;
+    }
+}
+
+long double exp(){
+    int i;
+    long double s = 0;
+    for(i = 0; i < 150; i++){
+        s = s + (pwr[i]/fact[i]);
+    }
+    return s;
+}
+
+
+int main(){
+    int i;
+    long double sum = 0;
+    printf("Exponential: Enter value of x (between 0 and 100): ");
+    scanf("%lf", &x);
+    #pragma omp parallel
+	{
+		omp_set_num_threads(2);
+        int total_threads = omp_get_num_threads();
+        printf(".....Creating Threads.....\n\n");
+        #pragma omp for
+		for(i = 0; i < total_threads; i++){
+			if(i == 0){
+                power();
+            }
+            if(i == 1){
+                factorial();
+            }
+        printf(".....Threads Created.....\n");
+		}
+        printf(".....Result Collected.....\n\n");
+    }
+    #pragma omp parallel master
+    {
+        sum = exp();
+    }
+    printf("Exponential: Value of %.2lf is: %lf\n\n", x, sum);
+	return 0;
+}
+
---
+++ PyMP and OpenMP/Q2_OpenMP.c
@@ -0,0 +1,60 @@
+#include <omp.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int count = 0;
+#define COUNT_DONE 10 
+#define COUNT_HALT1 3 
+#define COUNT_HALT2 6
+omp_lock_t lock;
+
+void funtion1(){
+    for(;;){
+        omp_init_lock(&lock);
+        while (count >= COUNT_HALT1 && count <= COUNT_HALT2){
+            sleep(1);
+        }
+        omp_set_lock(&lock);
+        count++;
+        printf("Function 1, Counter Value: %d\n", count);
+        omp_unset_lock(&lock);
+        if(count >= COUNT_DONE){
+            return NULL;
+        }
+    }
+}
+
+void function2(){
+    for(;;){
+        omp_init_lock(&lock);
+        while (count < COUNT_HALT1 || count > COUNT_HALT2){
+            sleep(1);
+        }
+        omp_set_lock(&lock);
+        count++;
+        printf("Function 2, Counter Value: %d\n", count);
+        omp_unset_lock(&lock);
+        if(count >= COUNT_DONE){
+            return NULL;
+        }
+    }
+}
+
+int main(){
+    int i = 0;
+    #pragma omp parallel
+    {   
+        omp_set_num_threads(2);
+        int total_threads = omp_get_num_threads();
+        #pragma omp for
+        for(i = 0; i < total_threads; i++){
+            if(i == 0){
+                function1();
+            }
+            if(i == 1){
+                function2();
+            }
+        }
+    }
+    return 0;
+}
