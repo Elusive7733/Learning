06b37a434cdb49426da95e1fe21b606bf0b8327f
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-02-06 13:50:21 +0500

    Vue

--- Not Languages/Vue.js/app.js
+++ Not Languages/Vue.js/app.js
@@ -1,13 +1,16 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
+
+
