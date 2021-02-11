02633e6dc67aa7199a741e50f51b42e90be4a80a
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-02-11 15:14:24 +0500

    Web Scrapping

---
+++ Not Languages/Web-Scrapping/test.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from shutil import which
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as expect
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+cpath = which("chromedriver")
+print(cpath)
+
+driver = webdriver.Chrome(cpath)
+driver.get("http://slate.nu.edu.pk/portal")
+wait = WebDriverWait(driver, 20)
+
+inputid = wait.until(expect.visibility_of_element_located((By.XPATH, "//input[@id='eid']")))
+inputpass = wait.until(expect.visibility_of_element_located((By.XPATH, "//input[@id='pw']")))
+submit_btn = wait.until(expect.visibility_of_element_located((By.XPATH, "//input[@id='submit']")))
+inputid.send_keys("K180170")
+inputpass.send_keys("doto4life")
+submit_btn.click()
