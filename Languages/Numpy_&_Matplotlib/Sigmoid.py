4910abc149be46d9502b711492d85c33cf013501
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-02-18 22:16:29 +0500

    Update Sigmoid.py

--- Languages/Numpy_&_Matplotlib/Sigmoid.py
+++ Languages/Numpy_&_Matplotlib/Sigmoid.py
@@ -1,34 +1,20 @@
-# import numpy as np
-# from matplotlib import pyplot as plt
-# import math
-
-# #sigmoid function
-# def sigmoid(num):
-#   return (1/(1 + np.exp(-num)))
-
-# x = np.random.sample(100)*5 + 1
-# print("x: ", x)
-# print("\n\n")
-# y = []
-
-# for i in range(len(x)):
-#   y.append(sigmoid(x[i]))
-    
-# print("y: ", y)
-
-
 import matplotlib.pyplot as plt 
 import numpy as np 
-import math 
+
+#sigmoid function
+def sigmoid(num):
+  return (1/(1 + np.exp(-num)))
+
+
   
 x = np.linspace(-10, 10, 100) 
-z = 1/(1 + np.exp(-x)) 
+y = []
+for i in range(len(x)):
+  y.append(sigmoid(x[i]))
   
-print("x: ", x)
-print("\n\n")
-print("z: ", z)
-plt.plot(x, z) 
+
+plt.plot(x, y, "*") 
 plt.xlabel("x") 
-plt.ylabel("Sigmoid(X)") 
+plt.ylabel("Sigmoid Function") 
   
 plt.show() 
