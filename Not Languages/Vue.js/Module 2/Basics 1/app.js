ddda7f16577c6bb3e725a2a0e9eea6a18399bc68
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-02-21 17:38:21 +0500

    a

--- Not Languages/Vue.js/Module 2/Basics 1/app.js
+++ Not Languages/Vue.js/Module 2/Basics 1/app.js
@@ -1,15 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { 
-      enteredGoalValue: '',
-      goals: []
-     };
+    return {
+      goals: [],
+      goal_data: "",
+    };
   },
   methods: {
     addGoal() {
-      this.goals.push(this.enteredGoalValue);
-    }
-  }
+      this.goals.push(this.goal_data);
+      this.goal_data = "";
+      console.log(this.goals);
+    },
+  },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
