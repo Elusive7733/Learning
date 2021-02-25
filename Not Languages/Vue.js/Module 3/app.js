469c52670f7110473ab53b677438a06dadddcaa5
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-02-25 16:49:28 +0500

    game complete

--- Not Languages/Vue.js/Module 3/app.js
+++ Not Languages/Vue.js/Module 3/app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
       round: 0,
       game_end: false,
       win: null,
+      log_messages: [],
     };
   },
   computed: {
@@ -58,25 +59,31 @@ const app = Vue.createApp({
       this.round++;
       let dmg = getRandom(3, 10);
       this.enemy_health -= dmg;
+      this.updateLog("Player", "Attacks", "Monster", dmg);
       this.defend();
     },
     defend() {
       let dmg = getRandom(7, 14);
       this.player_health -= dmg;
+      this.updateLog("Monster", "Attacks", "Player", dmg);
     },
     specialAttack() {
       this.round++;
       let dmg = getRandom(19, 19);
       this.enemy_health -= dmg;
+      this.updateLog("Player", "Ultimates", "Monster", dmg);
       this.defend();
     },
     heal() {
       this.round++;
-      let healValue = getRandom(20, 20);
-      if (this.player_health + healValue > 100) {
+      let heal_value = getRandom(11, 17);
+      if (this.player_health + heal_value > 100) {
+        heal_value = 100 - player_health;
         this.player_health = 100;
+        this.updateLog("Player", "Heals", "Player", heal_value);
       } else {
-        this.player_health += healValue;
+        this.player_health += heal_value;
+        this.updateLog("Player", "Heals", "Player", heal_value);
       }
       this.defend();
     },
@@ -86,10 +93,20 @@ const app = Vue.createApp({
       this.round = 0;
       this.game_end = false;
       this.win = null;
+      this.log_messages = [];
     },
     surrender() {
       this.game_end = true;
       this.win = false;
+    },
+    updateLog(who, what, whom, value) {
+      // we dont take whom because only 2 characters here
+      this.log_messages.unshift({
+        action_by: who,
+        action_type: what,
+        action_to: whom,
+        action_value: value,
+      });
     },
   },
 });
