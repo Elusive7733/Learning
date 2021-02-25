93f6f28e95487fc4c858eb431ad166fb2186b5ad
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-02-25 12:39:14 +0500

    a

--- Not Languages/Vue.js/Module 3/app.js
+++ Not Languages/Vue.js/Module 3/app.js
@@ -18,24 +18,51 @@ const app = Vue.createApp({
       return { width: this.player_health + "%" };
     },
     useSpecialAttack() {
-      return this.round % 3 !== 0;
+      return this.round % 3 !== 0; //this returns true or false
+    },
+  },
+  watch: {
+    //instead of making a lot of if statements in every function we can use watchers
+    player_health(value) {
+      if (value <= 0 && this.enemy_health <= 0) {
+        //a draw
+      } else if (value <= 0) {
+        //player lost
+      }
+    },
+    enemy_health(value) {
+      if (value <= 0 && this.player_health <= 0) {
+        //a draw
+      } else if (value <= 0) {
+        //player lost
+      }
     },
   },
   methods: {
     attack() {
       this.round++;
-      let dmg = getRandom(3, 20);
+      let dmg = getRandom(3, 10);
       this.enemy_health -= dmg;
       this.defend();
     },
     defend() {
-      let dmg = getRandom(8, 25);
+      let dmg = getRandom(7, 14);
       this.player_health -= dmg;
     },
     specialAttack() {
       this.round++;
-      let dmg = getRandom(10, 30);
+      let dmg = getRandom(9, 16);
       this.enemy_health -= dmg;
+      this.defend();
+    },
+    heal() {
+      this.round++;
+      let healValue = getRandom(10, 20);
+      if (this.player_health + healValue > 100) {
+        this.player_health = 100;
+      } else {
+        this.player_health += healValue;
+      }
       this.defend();
     },
   },
