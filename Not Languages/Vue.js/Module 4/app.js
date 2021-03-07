77041f5ffc0e64426c3fc458586b4c40dbd89e36
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-03-07 20:18:14 +0500

    a

--- Not Languages/Vue.js/Module 4/app.js
+++ Not Languages/Vue.js/Module 4/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      details_visible: false,
       friends: [
         {
           id: "Abdullah",
@@ -18,13 +17,37 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+//custom html elements (use dash to avoid clash for example dont name components h2)
+//mini apps
+app.component("friend-contact", {
+  template: `    
+    <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">
+            {{ details_visible ? 'Hide' : 'Show'}} Details
+          </button>
+          <ul v-if="details_visible">
+            <li><strong>Gamer Name:</strong> {{ friend.gamer_tag }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+    </li>`,
+  data() {
+    return {
+      details_visible: false,
+      friend: {
+        id: "Hiba",
+        name: "Hiba Abdullah",
+        gamer_tag: "Ms_birb",
+        email: "Hiba_razzaq00@gmail.com",
+      },
+    };
+  },
   methods: {
     toggleDetails() {
       this.details_visible = !this.details_visible;
     },
   },
 });
-
-//custom html elements (use dash to avoid clash for example dont name components h2)
-app.component("friend-contact");
 app.mount("#app");
