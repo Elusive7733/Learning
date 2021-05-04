1358b1c19d41b755ad6a0cf5c7dca0f27210c5ce
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-05-04 21:43:20 +0500

    a

--- Not Languages/React/react-basics/src/App.js
+++ Not Languages/React/react-basics/src/App.js
@@ -22,20 +22,31 @@ class App extends Component {
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+        persons: [
+          {name: "Abdullah", nick: "Abduls"},
+          {name: event.target.value, nick: "Hibs"},
+          {name: "Elusive", nick: "Elu" },
+        ]
+      })
+  }
+
   render() {
     return (
 
       <div className="App">
         <h1>Hello This is abdullah
           <p><em>This is Italic</em></p>
-          <button onClick={() => this.switchNameHandler("Bro")}>Switch Name</button>
+          <button onClick={this.switchNameHandler.bind(this, "Abdullah")}>Switch Name</button>
           <Person
             name={this.state.persons[0].name}
             nick={this.state.persons[0].nick} /> <br></br>
           <Person
             name={this.state.persons[1].name}
             nick={this.state.persons[1].nick}
-            click={this.switchNameHandler.bind(this, "Nibba")} >You like Sekiro</Person> <br></br>
+            click={this.switchNameHandler.bind(this, "Nibba")}
+            changed={this.nameChangeHandler} >You like Sekiro</Person> <br></br>
           <Person
             name={this.state.persons[2].name}
             nick={this.state.persons[2].nick}/> <br></br>
--- Not Languages/React/react-basics/src/Person/Person.js
+++ Not Languages/React/react-basics/src/Person/Person.js
@@ -6,6 +6,7 @@ const person = (props) => {
             <p onClick={props.click}>Hello There {props.name} you are also called { props.nick }</p>
             <p>You are {Math.floor(Math.random() * 30)} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     )
 }
