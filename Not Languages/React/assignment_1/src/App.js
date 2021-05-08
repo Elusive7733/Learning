63dcdcd72f03afbadbdc8e625abcb5e9dd96781e
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-05-08 08:56:56 +0500

    part 1 done

--- Not Languages/React/assignment_1/src/App.js
+++ Not Languages/React/assignment_1/src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 
 
 class App extends Component {
+  state = {
+    userInput: ''
+  };
+
+  updateInputHandler = (event) => {
+    this.setState({userInput: event.target.value})
+  };
+
   render(){
     return(
       <div className="App">
-        <h1>Hello This is abdullah</h1>
+          <input type="text" onChange={(event) => this.updateInputHandler(event)} value={this.state.userInput} />
+          <p>{this.state.userInput.length}</p>
       </div>
     );
   };
