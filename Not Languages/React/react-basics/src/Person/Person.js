a1cd5426749497eb9f8c3039e88c936e05fb42c7
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-05-15 01:43:22 +0500

    Update Person.js

--- Not Languages/React/react-basics/src/Person/Person.js
+++ Not Languages/React/react-basics/src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Radium from 'radium'
+// import Radium from 'radium'
 // import './Person.css'
 import styled from 'styled-components';
 
@@ -34,4 +34,4 @@ const person = (props) => {
 }
 
 
-export default Radium(person); 
+export default person; 
