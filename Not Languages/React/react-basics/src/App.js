87c3f7087f72008aaff0f72a5a9e85f86646bee7
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-05-14 16:37:31 +0500

    Update App.js

--- Not Languages/React/react-basics/src/App.js
+++ Not Languages/React/react-basics/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -82,15 +82,17 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hello This is abdullah</h1>
-          <p className={classes.join(' ')}><em>This is Italic</em></p>
-          <button
-            style={style}
-            onClick={this.togglePersonHander}
-            >Display Persons</button>
-        {persons}
-      </div> 
+      <StyleRoot>
+        <div className="App">
+          <h1>Hello This is abdullah</h1>
+            <p className={classes.join(' ')}><em>This is Italic</em></p>
+            <button
+              style={style}
+              onClick={this.togglePersonHander}
+              >Display Persons</button>
+          {persons}
+        </div> 
+      </StyleRoot>
     );
   }
 }
