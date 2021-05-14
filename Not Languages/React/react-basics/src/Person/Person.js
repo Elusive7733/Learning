132ed8cf2bd65dba3f9c11b35d24187e0ff200e2
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-05-14 20:17:42 +0500

    Dynamic styling

--- Not Languages/React/react-basics/src/Person/Person.js
+++ Not Languages/React/react-basics/src/Person/Person.js
@@ -4,12 +4,18 @@ import './Person.css'
 
 const person = (props) => {
     const style = {
-        '@media (min-width: 500px)': {width: '450px'}
+        '@media (min-width: 500px)': {width: '450px'},
+        ':hover': {     
+            backgroundColor: 'Azure',
+            transition: 'transform 0.5s',
+            boxShadow: '0px 0px 5px rgb(190, 255, 255), inset 0px 0px 5px rgb(190, 255, 255)',
+            transform: 'scale(1.08)',
+        }
     } 
 
     return(
-        <div className="Person" style={style}>
-            <p onClick={props.click}>Hello There {props.name} you are also called { props.nick }</p>
+        <div className="Person" style={style} onClick={props.click}>
+            <p>Hello There {props.name} you are also called { props.nick }</p>
             <p>You are {Math.floor(Math.random() * 30)} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
