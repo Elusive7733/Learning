cb50ca818dabaf2ec037b8d042bea2e46f958da6
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-05-15 11:36:24 +0500

    CSS Modules working

--- Not Languages/React/react-basics/src/Person/Person.js
+++ Not Languages/React/react-basics/src/Person/Person.js
@@ -1,34 +1,15 @@
 import React from 'react'
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-width: 60%;
-margin: 16px auto;
-border: 1px solid grey;
-box-shadow: 0 2px 3px #ccc;
-padding: 16px;
-text-align: center;
-cursor: pointer;
-
-@media (min-width: 500px) { width: 450px;}
-&:hover{     
-    backgroundColor: Azure;
-    transition: transform 0.5s;
-    box-shadow: 0px 0px 5px rgb(190, 255, 255), inset 0px 0px 5px rgb(190, 255, 255);
-    transform: scale(1.08);
-}
-`
+import classes from './Person.css'
 
 const person = (props) => {
     return(
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>Hello There {props.name} you are also called { props.nick }</p>
             <p>You are {Math.floor(Math.random() * 30)} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
-        </StyledDiv>
-    )
+        </div>
+    );
 }
-
 
 export default person; 
