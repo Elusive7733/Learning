430bd794b6ec6917938c3ac170c326c7c5b9056f
Abdullah Raheel <Abdullaharaheel@gmail.com>
2021-09-28 19:09:36 +0500

    tupples and array

--- Languages/TypeScript/dist/index.js
+++ Languages/TypeScript/dist/index.js
@@ -8,3 +8,7 @@ xyz = true; //not an error because of :any type
 // __________________Arrays__________________
 let names = ["Abdullah", "Elusive", "Raheel"]; // string[] tells the compiler that each element must be a string
 // names.push(12);
+let test = [1, "Hello", true];
+// __________________Tupples__________________
+let person = [22, "Hiba", true];
+// __________________ __________________
--- Languages/TypeScript/src/index.ts
+++ Languages/TypeScript/src/index.ts
@@ -8,6 +8,14 @@ let xyz: any = "what";
 xyz = true; //not an error because of :any type
 
 // __________________Arrays__________________
-let names: string[] = ["Abdullah", "Elusive", "Raheel"]; // string[] tells the compiler that each element must be a string
 
+let names: string[] = ["Abdullah", "Elusive", "Raheel"]; // string[] tells the compiler that each element must be a string
 // names.push(12);
+
+let test: any[] = [1, "Hello", true];
+
+// __________________Tupples__________________
+
+let person: [number, string, boolean] = [22, "Hiba", true];
+
+// __________________ __________________
