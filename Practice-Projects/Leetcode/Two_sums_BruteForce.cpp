311279cbcafb8562470a98f5dc88987457480e00
Abdullah Raheel <Abdullaharaheel@gmail.com>
2022-01-28 17:07:47 -0500

    sum changes

--- Practice-Projects/Leetcode/Two_sums_BruteForce.cpp
+++ Practice-Projects/Leetcode/Two_sums_BruteForce.cpp
@@ -9,7 +9,7 @@ public:
         vector<int> temp;
         for(int i = 0; i < nums.size(); i++){
             for(int j = i+1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == tar){
+                if(nums[i] + nums[j] == tar){ 
                     temp.push_back(i);
                     temp.push_back(j);
                     return temp;
